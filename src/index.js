40583d171372d2859038de487779b30b08a89c90
Sampretha <99082229+Sampretha@users.noreply.github.com>
2023-08-24 19:34:04 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  <div>
+    <h1>Hello world!</h1>
+    <p>welcome all</p>
+  </div>,
+  document.getElementById("root")
+);
